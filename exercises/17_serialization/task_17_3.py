0c6dd5a2dfc9ccd7591300ef77a9cd4a610a1b12
vbrol <seva.brol@gmail.com>
2021-07-13 20:13:33 +0300

    17.3

--- exercises/17_serialization/task_17_3.py
+++ exercises/17_serialization/task_17_3.py
@@ -24,10 +24,14 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 """
+import re
 
 def parse_sh_cdp_neighbors(cdpne):
     hostname=re.search("^(\S+)['#'|'>']",cdpne).group(1)
     regex=r"(?<rr>\S+) +(?P<iface>\S+ \S+) +\d+.* (?P<riface>\S+ \S+)\n"
+    match = re.finditer(regexp,cdpne)
+    for m in match:
+
 
 
 
